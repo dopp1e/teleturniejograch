be392b013742003a764840a3c0abdb7227453a45
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-07-31 23:33:57 +0200

    feat(team): add game related fields, score, keyAmt, spells

--- src/lib/teams.ts
+++ src/lib/teams.ts
@@ -1,8 +1,13 @@
+import type { Spell }  from '@/lib/spell'
+
 export interface Team {
   id: string;
   name: string;
   members: string[];
   hexColor: string;
+  score: number | null;
+  keyAmt: number | null;
+  spells: Spell[] | null;
 }
 
 export interface Result {
